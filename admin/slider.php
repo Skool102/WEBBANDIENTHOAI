92f4d59b4d2dd5a3c40c3297b77899617c25595e
Skool <mjtdzzz@gmail.com>
2023-11-07 22:37:37 +0700

    first commit hoidanit

---
+++ admin/slider.php
@@ -0,0 +1,20 @@
+<section class="admin-content">
+        <div class="admin-content-left">
+            <ul>
+                <li><a href="#">Danh mục</a></li>
+                    <ul>
+                        <li><a href="cartegoryadd.php">Thêm danh mục</a></li>
+                        <li><a href="cartegorylist.php">Danh sách danh mục</a></li>
+                    </ul>
+                <li><a href="#">Loại sản phẩm</a></li>
+                    <ul>
+                        <li><a href="brandadd.php">Thêm loại sản phẩm</a></li>
+                        <li><a href="brandlist.php">Danh sách sản phẩm</a></li>
+                    </ul>
+                <li><a href="#">Sản phẩm</a></li>
+                    <ul>
+                        <li><a href="">Thêm sản phẩm</a></li>
+                        <li><a href="">Danh sách sản phẩm</a></li>
+                    </ul>
+            </ul>
+        </div>
